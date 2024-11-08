bd504f0c272d726647492124b92d69b35b88bbc6
Arturo Ramirez <ramirez.garturo222.1922@gmail.com>
2024-11-07 23:02:27 -0600

    pruebaservidorabajobd

--- QuizzVitaProyecto/QuizzDepresion/Depresion.aspx.cs
+++ QuizzVitaProyecto/QuizzDepresion/Depresion.aspx.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace QuizzVitaProyecto.QuizzDepresion
 {
@@ -11,12 +12,13 @@ namespace QuizzVitaProyecto.QuizzDepresion
         protected void Page_Load(object sender, EventArgs e)
         {
         }
+
         protected void SubmitQuiz(object sender, EventArgs e)
         {
             // FUNCIONALIDAD PARA OBTENER RESULTADOS DEL QUIZZ ---
             int totalScore = 0;
 
-            // Guardar las respuestas en un arreglo
+            // Guardar las respuestas en una lista
             List<int> respuestas = new List<int>();
             respuestas.Add(Convert.ToInt32(Request.Form["q1"]));
             respuestas.Add(Convert.ToInt32(Request.Form["q2"]));
@@ -37,7 +39,7 @@ namespace QuizzVitaProyecto.QuizzDepresion
 
             // Determinar el diagnóstico
             string diagnosis = "";
-            if (totalScore <= 10) // Ajusta estos valores según la cantidad de preguntas
+            if (totalScore <= 10)
             {
                 diagnosis = "Depresion Baja";
             }
@@ -50,10 +52,26 @@ namespace QuizzVitaProyecto.QuizzDepresion
                 diagnosis = "Depresion Severa";
             }
 
+            // Insertar el resultado en la base de datos
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO [dbo].[results] (user_id, problem_id, fecha, puntaje) VALUES (@UserID, @ProblemID, @Fecha, @Puntaje)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserID", 1);  // Reemplaza este valor con el ID del usuario
+                    command.Parameters.AddWithValue("@ProblemID", 1); // ID del quiz "Depresión" (1)
+                    command.Parameters.AddWithValue("@Fecha", DateTime.Now);
+                    command.Parameters.AddWithValue("@Puntaje", totalScore);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
             // Redirigir a la página de resultados con el diagnóstico
             Response.Redirect($"Res.aspx?diagnosis={diagnosis}&score={totalScore}");
         }
     }
-
-  
 }
